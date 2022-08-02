3a43e5fa99a98184571af35335c40a3331380b77
MinseokChoi12 <minseok1268@gmail.com>
2022-08-02 22:19:01 +0900

    BFS Practice

--- Exercise_Graph/Program.cs
+++ Exercise_Graph/Program.cs
@@ -18,8 +18,8 @@ namespace Exercise_Graph
                 { 0, 1, 0, 1, 0, 0 },
                 { 1, 0, 1, 1, 0, 0 },
                 { 0, 1, 0, 0, 0, 0 },
-                { 1, 1, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 0, 0, 1, 0 },
+                { 0, 0, 0, 1, 0, 1 },
                 { 0, 0, 0, 0, 1, 0 }
         };
 
@@ -29,52 +29,40 @@ namespace Exercise_Graph
                 new List<int>() { 1, 3 },
                 new List<int>() { 0, 2, 3 },
                 new List<int>() { 1 },
-                new List<int>() { 0, 1 },
-                new List<int>() { 5 },
+                new List<int>() { 0, 1, 4 },
+                new List<int>() { 3, 5 },
                 new List<int>() { 4 }
         };
 
-        bool[] visited = new bool[6];
-
-        // 1) 우선 now부터 방문
-        // 2) now와 연결된 정점들을 하나씩 확인해서, 아직 미발견 상태라면 방문
-        public void DFS(int now)
+        public void BFS(int start)
         {
-            Console.WriteLine(now);
-            visited[now] = true;    // 1)
+            bool[] found = new bool[6];
+            // **** 추가정보 ****
+            int[] parent = new int[6]; // 어느 방에서 왔는지
+            int[] distance = new int[6]; // 이동거리
 
-            // 행렬
-            for (int next = 0; next < 6; next++)
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;
+
+            while (q.Count > 0)
             {
-                if (adj[now, next] == 0)
-                    continue;
-                if (visited[next])
-                    continue;
-                DFS(next);
-            }
-        }
+                int now = q.Dequeue();
+                Console.WriteLine(now);
 
-        // index 이용
-        public void DFS2(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true;
-
-            foreach (int next in adj2[now])
-            {
-                if (visited[next])
-                    continue;
-                DFS2(next);
-            }
-        }
-
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-            {
-                if(visited[now] == false)
-                    DFS(now);
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0) // 인접하지 않았으면 스킵
+                        continue;
+                    if (found[next]) // 이미 발견한 방이면 스킵
+                        continue;
+                    q.Enqueue(next);
+                    found[next] = true;
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1;
+                }
             }
         }
     }
@@ -84,9 +72,9 @@ namespace Exercise_Graph
         static void Main(string[] args)
         {
             // DFS (Depth First Search, 깊이 우선 탐색)
-            // BFS (Breadth First Search, 너비 우선 탐색)
+            // BFS (Breadth First Search, 너비 우선 탐색) -> 거의 길찾기(최단거리)에서만 사용
             Graph graph = new Graph();
-            graph.SearchAll();
+            graph.BFS(1);
         }
     }
 }
