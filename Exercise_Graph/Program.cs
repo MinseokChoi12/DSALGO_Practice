50d9bf2d1d16bb682af4bbfca82113d0ceab7a20
MinseokChoi12 <minseok1268@gmail.com>
2022-08-05 12:33:57 +0900

    Dijikstra Algorithm

--- Exercise_Graph/Program.cs
+++ Exercise_Graph/Program.cs
@@ -15,53 +15,74 @@ namespace Exercise_Graph
         // 행렬이용
         int[,] adj = new int[6, 6]
         {
-                { 0, 1, 0, 1, 0, 0 },
-                { 1, 0, 1, 1, 0, 0 },
-                { 0, 1, 0, 0, 0, 0 },
-                { 1, 1, 0, 0, 1, 0 },
-                { 0, 0, 0, 1, 0, 1 },
-                { 0, 0, 0, 0, 1, 0 }
+                { -1, 15, -1, 35, -1, -1 },
+                { 15, -1, 05, 10, -1, -1 },
+                { -1, 05, -1, -1, -1, -1 },
+                { 35, 10, -1, -1, 05, -1 },
+                { -1, -1, -1, 05, -1, 05 },
+                { -1, -1, -1, -1, 05, -1 }
         };
 
-        // 필요한 index 정보만
-        List<int>[] adj2 = new List<int>[]
+        // 다익스트라 알고리즘
+        // 가중치가 있을 때 사용
+        // 가중치의 합이 가장 작은 경로(ex. 가장 가까운 거리, 시간이 덜 걸리는 거리)를 찾는다.
+        public void Dijikstra(int start)
         {
-                new List<int>() { 1, 3 },
-                new List<int>() { 0, 2, 3 },
-                new List<int>() { 1 },
-                new List<int>() { 0, 1, 4 },
-                new List<int>() { 3, 5 },
-                new List<int>() { 4 }
-        };
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+            Array.Fill(distance, Int32.MaxValue);
 
-        public void BFS(int start)
-        {
-            bool[] found = new bool[6];
-            // **** 추가정보 ****
-            int[] parent = new int[6]; // 어느 방에서 왔는지
-            int[] distance = new int[6]; // 이동거리
-
-            Queue<int> q = new Queue<int>();
-            q.Enqueue(start);
-            found[start] = true;
-            parent[start] = start;
             distance[start] = 0;
+            parent[start] = start;
 
-            while (q.Count > 0)
+            while (true)
             {
-                int now = q.Dequeue();
-                Console.WriteLine(now);
+                // 제일 좋은 후보를 찾는다(가장 가까이에 있는 후보)
 
+                // 가장 유력한 후보의 거리와 번호를 저장한다
+                int closest = Int32.MaxValue;
+                int now = -1;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    // 이미 방문한 정점은 스킵
+                    if (visited[i])
+                        continue;
+                    // 아직 발견된 적이 없거나, 기존 후보보다 멀리 있으면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+                    // 후보 갱신
+                    closest = distance[i];
+                    now = i;
+                }
+
+                // 다음 후보가 없다 -> 종료
+                if (now == -1)
+                    break;
+
+                // 제일 좋은 후보를 찾았으니까 방문
+                visited[now] = true;
+
+                // 방문한 정점과 인접한 정점들을 조사해서, 상황에 따라 발견한 최단거리를 갱신한다.
                 for (int next = 0; next < 6; next++)
                 {
-                    if (adj[now, next] == 0) // 인접하지 않았으면 스킵
+                    // 연결되지 않은 정점 스킵
+                    if (adj[now, next] == -1)
                         continue;
-                    if (found[next]) // 이미 발견한 방이면 스킵
+                    // 이미 방문한 정점은 스킵
+                    if (visited[next])
                         continue;
-                    q.Enqueue(next);
-                    found[next] = true;
-                    parent[next] = now;
-                    distance[next] = distance[now] + 1;
+
+                    // 새로 조사된 정점의 최단거리를 계산하다.
+                    int nextDist = distance[now] + adj[now, next];
+
+                    // 만약 기존에 발견한 최단거리가 새로 조사된 최단거리보다 크면, 정보를 갱신
+                    if (nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
                 }
             }
         }
@@ -74,7 +95,7 @@ namespace Exercise_Graph
             // DFS (Depth First Search, 깊이 우선 탐색)
             // BFS (Breadth First Search, 너비 우선 탐색) -> 거의 길찾기(최단거리)에서만 사용
             Graph graph = new Graph();
-            graph.BFS(1);
+            graph.Dijikstra(1);
         }
     }
 }
